4530de14ed5b59673331d96c71118697cebf4a20
lange <lange@synyx.de>
2023-01-18 22:44:38 +0100

    example but not nice

--- validation/src/main/java/com/example/validation/ExampleController.java
+++ validation/src/main/java/com/example/validation/ExampleController.java
@@ -1,34 +1,46 @@
 package com.example.validation;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.regex.Pattern;
+
 @Controller
 public class ExampleController {
 
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("form", new Form(""));
         return "example";
     }
 
     record Form(
-            @Email String email) {
+            String email) {
     }
 
     @PostMapping("/check")
-    public String check(Form form, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
+    public String check(@Valid Form form, BindingResult bindingResult, Model model) {
+
+        if (!VALID_EMAIL_ADDRESS_REGEX.matcher(form.email).find()) {
+            bindingResult.addError(new ObjectError("email", "email not valid"));
+            model.addAttribute("errors", new ObjectError("email", "email not valid"));
             return "example";
         }
-        return "redirect:/valid";
+        return "redirect:/valid?validInput=" + form.email;
     }
 
     @GetMapping("/valid")
-    public String valid() {
+    public String valid(@RequestParam("validInput") String validInput, Model model) {
+        model.addAttribute("validInput", validInput);
         return "valid";
     }
 }
