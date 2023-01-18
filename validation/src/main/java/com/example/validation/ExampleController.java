a498359d189ea95f6f01ec85acb643c3ba9b7a7d
lange <lange@synyx.de>
2023-01-18 22:26:24 +0100

    checking email example

--- validation/src/main/java/com/example/validation/ExampleController.java
+++ validation/src/main/java/com/example/validation/ExampleController.java
@@ -1,14 +1,34 @@
 package com.example.validation;
 
+import jakarta.validation.constraints.Email;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ExampleController {
 
     @GetMapping("/")
     public String main() {
-
         return "example";
+    }
+
+    record Form(
+            @Email String email) {
+    }
+
+    @PostMapping("/check")
+    public String check(Form form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "example";
+        }
+        return "redirect:/valid";
+    }
+
+    @GetMapping("/valid")
+    public String valid() {
+        return "valid";
     }
 }
