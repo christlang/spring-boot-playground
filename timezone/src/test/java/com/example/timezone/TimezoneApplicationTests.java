3994f39b8d4f87942391a20f0c93e4f068be6dff
lange <lange@synyx.de>
2023-01-24 16:57:21 +0100

    initial commit timezone

---
+++ timezone/src/test/java/com/example/timezone/TimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
