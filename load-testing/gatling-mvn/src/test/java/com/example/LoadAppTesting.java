a020f28cae8d3953e283dae198d9208b5fe84f65
lange <lange@synyx.de>
2023-02-09 20:27:50 +0100

    added example of session parameter changes

--- load-testing/gatling-mvn/src/test/java/com/example/LoadAppTesting.java
+++ load-testing/gatling-mvn/src/test/java/com/example/LoadAppTesting.java
@@ -3,6 +3,8 @@ package com.example;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+import java.time.Duration;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
@@ -17,12 +19,39 @@ public class LoadAppTesting extends Simulation {
             exec(http("get index")
                     .get(""));
 
+    final private static ChainBuilder post =
+            exec(http("submit")
+                    .post("/submit")
+                    .check(currentLocation().saveAs("responseUrl"))
+            );
+
+    final private static ChainBuilder getResult =
+            exec(http("get response #{responseUrl}")
+                    .get("#{responseUrl}")
+                    .check(substring("result: <span>1</span>").find().exists())
+            );
+
+
 
     final private ScenarioBuilder scn = scenario("produce some load")
             .forever().on(
                     exec(getIndex)
                             .pause(5)
                             .exec(getIndex)
+                            .pause(5)
+                            .exec(post)
+                            .pause(Duration.ofMillis(10))
+                            .exec(
+                                    session -> {
+                                        String before = session.get("responseUrl").toString();
+                                        String after = before.replaceAll("id=\\d+", "id=1");
+                                        session.remove("responseUrl");
+                                        // System.out.println("responseUrl before: " + before);
+                                        // System.out.println("responseUrl after : " + after);
+                                        return session.set("responseUrl", after);
+                                    }
+                            )
+                            .exec(getResult)
             );
 
 
@@ -30,7 +59,7 @@ public class LoadAppTesting extends Simulation {
         setUp(
                 scn.injectOpen(
                         nothingFor(5),
-                        rampUsers(10_000).during(60)
+                        rampUsers(10).during(60)
                 ).protocols(httpProtocol)
         ).maxDuration(60);
     }
--- load-testing/load-app/src/main/java/com/example/load/app/LoadAppController.java
+++ load-testing/load-app/src/main/java/com/example/load/app/LoadAppController.java
@@ -1,7 +1,10 @@
 package com.example.load.app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoadAppController {
@@ -9,5 +12,16 @@ public class LoadAppController {
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/submit")
+    public String submit() {
+        return "redirect:/result?id=" + System.currentTimeMillis();
+    }
+
+    @GetMapping("/result")
+    public String result(@RequestParam String id, Model model) {
+        model.addAttribute("currentTimeMillis", id);
+        return "result";
     }
 }
