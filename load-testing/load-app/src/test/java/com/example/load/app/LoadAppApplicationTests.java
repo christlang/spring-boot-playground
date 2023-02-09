a29a19131a14c2301d718c121942874ae576cab4
lange <lange@synyx.de>
2023-02-09 14:43:38 +0100

    initial load testing commit

---
+++ load-testing/load-app/src/test/java/com/example/load/app/LoadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.load.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
