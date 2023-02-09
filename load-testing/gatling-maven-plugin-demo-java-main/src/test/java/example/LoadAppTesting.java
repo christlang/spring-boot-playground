8bf16861ce30631cd182c1ae92affa780ed77e2b
lange <lange@synyx.de>
2023-02-09 15:04:54 +0100

    Load testing with 10.000 users

---
+++ load-testing/gatling-maven-plugin-demo-java-main/src/test/java/example/LoadAppTesting.java
@@ -0,0 +1,37 @@
+package example;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class LoadAppTesting extends Simulation {
+
+    final private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080")
+            .acceptHeader("text/html");
+
+
+    final private static ChainBuilder getIndex =
+            exec(http("get index")
+                    .get(""));
+
+
+    final private ScenarioBuilder scn = scenario("produce some load")
+            .forever().on(
+                exec(getIndex)
+                    .pause(5)
+                    .exec(getIndex)
+            );
+
+
+    {
+        setUp(
+                scn.injectOpen(
+                        nothingFor(5),
+                        rampUsers(10_000).during(60)
+                ).protocols(httpProtocol)
+        ).maxDuration(60);
+    }
+}
