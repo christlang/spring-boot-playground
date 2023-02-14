0cec5b42cdf1cef116a576497adc1e5b7c6f0eee
lange <lange@synyx.de>
2023-02-14 20:05:34 +0100

    initial commit for spring oauth jwt example

---
+++ spring-oauth-jwt/src/main/java/com/example/springoauthjwt/ExampleController.java
@@ -0,0 +1,14 @@
+package com.example.springoauthjwt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ spring-oauth-jwt/src/test/java/com/example/springoauthjwt/SpringOauthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
