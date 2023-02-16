07a247ac5969f2f1eb6557beb493fe0f46a6f388
lange <lange@synyx.de>
2023-02-16 16:50:15 +0100

    fix tests

--- spring-oauth-jwt/src/test/java/com/example/springoauthjwt/SpringOauthJwtApplicationTests.java
+++ spring-oauth-jwt/src/test/java/com/example/springoauthjwt/SpringOauthJwtApplicationTests.java
@@ -3,19 +3,20 @@ package com.example.springoauthjwt;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+import org.webjars.NotFoundException;
 
-import static org.assertj.core.api.Assertions.*;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringOauthJwtApplicationTests {
@@ -24,26 +25,42 @@ class SpringOauthJwtApplicationTests {
 	private Integer port;
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
+	private final User hasRight = new User("brice", "brice");
+	private final User hasNotRight = new User("igor", "igor");
+
 	@Test
-	void contextLoads() throws JSONException, JsonProcessingException {
-		RestTemplate restTemplate = new RestTemplate();
-
-		final var headers = new HttpHeaders();
-		headers.setContentType(MediaType.TEXT_HTML);
-		headers.setBearerAuth(getAccessToken());
-
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
-
-		final var result = restTemplate.exchange("http://localhost:%s/greet".formatted(port),
-				HttpMethod.GET, request, String.class);
+	void checkCorrectRight() throws JsonProcessingException {
+		final var result = requestGreet(hasRight);
 
 		assertThat(result.getStatusCode().value()).isEqualTo(200);
 		assertThat(result.getBody()).isEqualTo(
 				"Hi brice! You are granted with: [offline_access, NICE, default-roles-example, uma_authorization].");
 	}
 
+	@Test
+	void checkPermissionDenied() throws JsonProcessingException {
+		final var result = requestGreet(hasNotRight);
 
-	private String getAccessToken() throws JsonProcessingException {
+		assertThat(result.getStatusCode().value()).isEqualTo(403);
+	}
+
+	ResponseEntity<String> requestGreet(User user) throws JsonProcessingException {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
+
+		final var headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_HTML);
+		headers.setBearerAuth(getAccessToken(user));
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
+
+		return restTemplate.exchange("http://localhost:%s/greet".formatted(port),
+				HttpMethod.GET, request, String.class);
+	}
+
+	record User(String name, String pass) {}
+
+	private String getAccessToken(User user) throws JsonProcessingException {
 		RestTemplate restTemplate = new RestTemplate();
 
 		final var headers = new HttpHeaders();
@@ -52,8 +69,8 @@ class SpringOauthJwtApplicationTests {
 		form.add("grant_type", "password");
 		form.add("client_id", "spring-boot");
 		form.add("client_secret", "aweLEwtZ5H5rLVOQPQx2abuFocHtEv0v");
-		form.add("username", "brice");
-		form.add("password", "brice");
+		form.add("username", user.name);
+		form.add("password", user.pass);
 
 		HttpEntity<MultiValueMap<String, String>> request =
 				new HttpEntity<>(form, headers);
@@ -70,4 +87,32 @@ class SpringOauthJwtApplicationTests {
 
 		return accessToken;
 	}
+
+	static class RestTemplateResponseErrorHandler
+			implements ResponseErrorHandler {
+
+		@Override
+		public boolean hasError(ClientHttpResponse httpResponse)
+				throws IOException {
+
+			return (
+					httpResponse.getStatusCode().is4xxClientError()
+							|| httpResponse.getStatusCode().is5xxServerError());
+		}
+
+		@Override
+		public void handleError(ClientHttpResponse httpResponse)
+				throws IOException {
+
+			if (httpResponse.getStatusCode().is5xxServerError()) {
+				// handle SERVER_ERROR
+			} else if (httpResponse.getStatusCode().is4xxClientError()) {
+				// handle CLIENT_ERROR
+				if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+					throw new NotFoundException("not found");
+				}
+			}
+		}
+	}
+
 }
