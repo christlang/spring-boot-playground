c5bafa2e1c251f147161433a887bf99f72fb5003
lange <lange@synyx.de>
2023-02-28 06:53:47 +0100

    make single webclient call

---
+++ parallel-requests/src/main/java/com/example/parallelrequests/RequestController.java
@@ -0,0 +1,70 @@
+package com.example.parallelrequests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RequestController {
+
+    Logger log = LoggerFactory.getLogger(RequestController.class);
+
+    private static String URL = "https://www.google.de/";
+    private static int CALLS = 30;
+
+    @GetMapping
+    String index() {
+        return "index";
+    }
+
+    @GetMapping("webclient")
+    String webclient(Model model) {
+        model.addAttribute("duration", 10);
+
+        WebClient webClient = WebClient.create(URL);
+
+        long start = System.currentTimeMillis();
+
+        String result = webClient.get().exchangeToMono(response -> {
+            return Mono.just("result %s".formatted(response.statusCode()));
+        }).block();
+
+        long end = System.currentTimeMillis();
+        long diffInSeconds = end-start;
+        log.info("took %s ms".formatted(diffInSeconds));
+        model.addAttribute("duration", diffInSeconds);
+        model.addAttribute("results", result);
+
+        return "client-result";
+    }
+
+    @GetMapping("resttemplate")
+    String restclient(Model model) {
+        RestTemplate rest = new RestTemplate();
+        List<String> results = new ArrayList<>();
+
+        long start = System.currentTimeMillis();
+
+        for (int i=0; i < CALLS; ++i) {
+            results.add("result " + rest.getForEntity(URL, String.class).getStatusCode());
+        }
+
+        long end = System.currentTimeMillis();
+        long diffInSeconds = end-start;
+        log.info("took %s ms".formatted(diffInSeconds));
+        model.addAttribute("duration", diffInSeconds);
+        model.addAttribute("results", results);
+        return "client-result";
+    }
+}
