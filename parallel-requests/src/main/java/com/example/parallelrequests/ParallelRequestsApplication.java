18defe8633783a7a11bd6564b590b4f7ded92b6f
lange <lange@synyx.de>
2023-02-28 06:08:48 +0100

    initial commit parallel-requests

---
+++ parallel-requests/src/main/java/com/example/parallelrequests/ParallelRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.parallelrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelRequestsApplication.class, args);
+	}
+
+}
---
+++ parallel-requests/src/test/java/com/example/parallelrequests/ParallelRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parallelrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
